d0e2385bedec8907c9f81769341a39afb43cf20a
Malnati <ricardomalnati@gmail.com>
2024-10-16 16:41:07 -0300

    Update chart.ts

--- src/app/entities/chart.ts
+++ src/app/entities/chart.ts
@@ -51,6 +51,12 @@ updated_at: Date;
 @ApiProperty({ description: "Tipo de análise referente ao registro, como erosão, GEE, NH3, NPK, orgânicas, pesticidas ou poluição.",  })
 analysis: string;
 
+<<<<<<< Updated upstream
+=======
+	@Column({ type: 'varchar', length: 7, default: 'entrada' })
+	@ApiProperty({ description: "Tipo de transação, pode ser 'entrada' ou 'saída'." })
+	nutrient_flow: string;
+>>>>>>> Stashed changes
 
 
     toString() {
